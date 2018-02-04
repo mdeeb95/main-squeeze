0ff9a23e25ba6d19b180c0168b142a67937a4bc7
Logan <atmosphereasmr@gmail.com>
2018-02-03 19:17:04 -0800

    port change for the 093852th time

--- Server/server.js
+++ Server/server.js
@@ -10,6 +10,6 @@ app.get('*', (req, res)=>{
  res.sendFile(path.join(__dirname, '../build/index.html'));
 })
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 443;
 
 app.listen(port, () => { console.log(`Server listening on port ${port}.`); });
